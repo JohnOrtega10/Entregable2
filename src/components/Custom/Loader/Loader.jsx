5adedfc44f791bf92b84a836e77a8055af9c7922
JohnOrtega10 <johnortega.dev@gmail.com>
2022-01-04 00:11:45 -0500

    Mensaje del loader

--- src/components/Custom/Loader/Loader.jsx
+++ src/components/Custom/Loader/Loader.jsx
@@ -3,7 +3,13 @@ import './Loader.styles.css'
 
 const Loader = () => {
     return (
-        <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>    
+        <div className='loader'>
+            <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>    
+            <div className='message'>
+                <p>Grant location permissions to continue.</p>
+            </div>
+        </div>
+ 
     );
 };
 
